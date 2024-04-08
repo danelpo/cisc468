a674e936f78ec193839ec93db43244a79e6ac61b
LisPlakolli <plakollilis@gmail.com>
2024-04-08 01:23:06 -0400

    added RSA key pair for signatures

--- Java/src/main/KeyGeneration.java
+++ Java/src/main/KeyGeneration.java
@@ -5,6 +5,7 @@ import javax.crypto.*;
 public class KeyGeneration {
 
     private SecKey secKey;
+    private KeyPair rsaKeyPair;
 
     private KeyPairGenerator keyPairGen;
     private KeyAgreement keyAgreement;
@@ -14,12 +15,23 @@ public class KeyGeneration {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         keyGen.init(256, new SecureRandom());
         this.secKey = keyGen.generateKey();
+
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+        keyPairGen.initialize(2048);
+        this.rsaKeyPair = keyPairGen.generateKeyPair();
     }
 
     public SecKey getSecKey(){
         return this.secKey;
     }
 
+    public PrivateKey getPrivateKey() {
+        return this.rsaKeyPair.getPrivate();
+    }
+
+    public PublicKey getPublicKey() {
+        return this.rsaKeyPair.getPublic();
+    }
 
     public void keyRegen() {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
